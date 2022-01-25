9ac170bb06ec1575a954dc58619810d79f960681
Diego Sottani <diegosottani@gmail.com>
2022-01-24 22:55:33 -0300

    Aplicação simples contruída

---
+++ src/pages/Home.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react'
+import { 
+    SafeAreaView, Text, StyleSheet, TextInput, Platform, FlatList 
+} from 'react-native'
+import { Button } from '../components/Button'
+import { SkillCard } from '../components/SkillCard'
+
+interface SkillData {
+    id: string,
+    name: string,
+    date?: Date,
+}
+
+export function Home(){
+    const [newSkill, setNewSkill] = useState('')
+    const [mySkills, setMySkills] = useState<SkillData[]>([])
+    const [greeting, setGreeting] = useState('')
+
+    function handleAddNewSkill(){
+        const data = {
+            id: String(new Date().getTime()),
+            name: newSkill
+        }
+
+        setMySkills(oldState => [...oldState, data])
+        // Como mySkills é um array, então faz spread
+        //setMySkills([...mySkills, newSkill])
+    }
+
+    function handleRemoveSkill(id: string){
+        setMySkills(oldState => oldState.filter(
+            skill => skill.id !== id
+        ))
+    }
+
+    useEffect(() => {
+        const currentHour = new Date().getHours()
+        if(currentHour < 12){
+            setGreeting('Good morning')
+        }else if(currentHour >= 12 && currentHour < 18){
+            setGreeting('Good afternoon')
+        }else{
+            setGreeting('Good evening')
+        }
+    }, [])
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Welcome, Diego</Text>
+            
+            <Text style={styles.greetings}>
+                {greeting}
+            </Text>
+
+            <TextInput 
+                style={styles.input}
+                placeholder="New skill"
+                placeholderTextColor="#555"
+                onChangeText={setNewSkill}
+            />
+
+            <Button onPress={handleAddNewSkill} title="Add" />
+
+            <Text style={[styles.title, {marginVertical: 50}]}>
+                My Skills
+            </Text>
+
+
+            <FlatList 
+                data={mySkills}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => (
+                    <SkillCard 
+                        skill={item.name}
+                        onPress={() => handleRemoveSkill(item.id)}
+                    />
+                )}
+            />               
+            
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingHorizontal: 30,
+        paddingVertical: 70
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7
+    },
+    greetings: {
+        color: '#FFF'
+    } 
+})
